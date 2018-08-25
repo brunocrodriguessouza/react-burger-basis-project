fa87a2ff9db6cfd22a0ed0e88b8126c02e086175
Bruno Souza <brunoprogramadorjava@gmail.com>
2018-08-24 23:52:41 -0300

    Creating a Layout Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <Layout>
+          <p>Teste Bruno</p>
+        </Layout>
       </div>
     );
   }
